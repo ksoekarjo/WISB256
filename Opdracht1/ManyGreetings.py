be694b7ea12a2dfea75863e91d42ec7614dd4cda
ksoekarjo <kasper.soekarjo@gmail.com>
2015-04-23 13:18:27 +0200

    Update ManyGreetings.py

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-name = input('What is your name?\n')
-print ('Hello, ' + name + '!')
-=======
 name = input('What is your name?\n')
 number_of_greetings = input('How many greetings?\n')
 print (number_of_greetings+ ' x '+'Hello, ' + name + '!')
->>>>>>> extension
+
 
 
