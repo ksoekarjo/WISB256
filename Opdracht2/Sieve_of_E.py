7ec2c207ba77885218e48a51c1f975ca92226335
ksoekarjo <kasper.soekarjo@gmail.com>
2015-04-30 14:38:25 +0200

    Update Sieve_of_E.py

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -1,7 +1,10 @@
+import sys
+print(str(sys.argv))
+
 import time
 T1 = time.perf_counter()
 
-N=int(input('Value for N?\n'))
+N=int(sys.argv[1])
 numbers=list(range(N))
 del numbers[0]
 del numbers[0]
@@ -17,5 +20,6 @@ T2 = time.perf_counter()
 fout=open('prime.dat','w')
 fout.write("\n".join(str(i) for i in numbers))
 fout.close()
-print('Found', len(numbers), 'Prime numbers smaller than', N, 'in', T2-T1, 'sec.')
+print('Found', len(numbers), 'Prime numbers smaller than', N, 'in', T2 - T1, 'sec.')
+
 
